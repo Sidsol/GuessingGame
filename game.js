3a01f52d85ebcf844c85002480fbfc5a77bfcf62
Jonah Martinez <Jonah.Martinez01@outlook.com>
2018-11-16 20:37:15 -0500

    Enhanced User experience by informing user when the game has ended.

--- game.js
+++ game.js
@@ -1,9 +1,11 @@
 // program data:
 var number = 4;
+var gameStatus = false;
 
 //prompt the user for the number of guesses they would like
 var numberOfGuesses = prompt("How many guesses would you like: ")
 
+//check for user input, if no input set numberOfGuesses to default state
 if (numberOfGuesses == '') {
     //default number of guess attempts
     numberOfGuesses = 5;
@@ -19,11 +21,15 @@ for (guess = 0; guess < numberOfGuesses; guess++)
     // if correct: let the user know they won
     if (userGuess == number) {
         document.write("Correct! You guessed the number.");
-        
+        gameStatus = true;
         break;
     }
     // if incorrect: let the user know
     else {
         alert("Sorry you did not guess correctly. \nTries remaining " + (numberOfGuesses - guess));
     }
+}
+if (!gameStatus)
+{
+    document.write("Sorry, you ran out of tries. Game over.");
 }
