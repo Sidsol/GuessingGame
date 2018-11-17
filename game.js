328eac5e1865ae2c5bf3b1b856cae0e713ffd080
Jonah Martinez <Jonah.Martinez01@outlook.com>
2018-11-16 20:30:53 -0500

    Added for loop logic to keep the game playing till user wins or looses.

---
+++ game.js
@@ -0,0 +1,29 @@
+// program data:
+var number = 4;
+
+//prompt the user for the number of guesses they would like
+var numberOfGuesses = prompt("How many guesses would you like: ")
+
+if (numberOfGuesses == '') {
+    //default number of guess attempts
+    numberOfGuesses = 5;
+}
+alert("You will have " + numberOfGuesses + " chances to guess the number");
+
+//for loop to determine number of attemps the player has left
+for (guess = 0; guess < numberOfGuesses; guess++) 
+{
+    // prompt user for their guess 
+    var userGuess = prompt("Please guess a number: ");
+
+    // if correct: let the user know they won
+    if (userGuess == number) {
+        document.write("Correct! You guessed the number.");
+        
+        break;
+    }
+    // if incorrect: let the user know
+    else {
+        alert("Sorry you did not guess correctly. \nTries remaining " + (numberOfGuesses - guess));
+    }
+}
